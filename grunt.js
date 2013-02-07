d91eafcd862780fce71d8b76446fc993f6ead625
Leonardo Quixada <lquixada@gmail.com>
2013-02-07 16:35:19 -0200

    grunt.js refactored.

--- grunt.js
+++ grunt.js
@@ -32,7 +32,7 @@ module.exports = function ( grunt ) {
 
     zip: {
       dist: {
-        exclude: ['./.git*', './node_modules*', './screenshots*'],
+        exclude: ['./.*', './node_modules*', './screenshots*'],
         src: '.',
         dest: ''+projectName+'.zip'
       }
@@ -51,47 +51,50 @@ module.exports = function ( grunt ) {
 
   
   grunt.registerMultiTask('imgs', 'Copy images to the build folder', function () {
-      var done = this.async();
+    var done = this.async();
 
-      if (fs.existsSync(this.data.src)) {
-          grunt.utils.spawn({ cmd: 'cp', args: ['-R', this.data.src, this.data.dest]}, function (err, result, code) {
-              if ( result.stderr ) {
-                  grunt.log.writeln( '\n'+result.stderr+'\n' );
-              } else {
-                  grunt.log.writeln('Image files copied successfully.');
-              }
-              
-              done(code>0? false: true);
-          } );
-      } else {
-          grunt.log.writeln('Nothing to do.');
-      }
+    if (fs.existsSync(this.data.src)) {
+      grunt.utils.spawn({ cmd: 'cp', args: ['-R', this.data.src, this.data.dest]}, function (err, result, code) {
+				grunt.helper('writeOutput', result.err, 'Image files copied successfully.', code);
+        
+        done(code>0? false: true);
+      } );
+    } else {
+      grunt.log.writeln('Nothing to do.');
+    }
   } );
 
 
   grunt.registerMultiTask('zip', 'Creates package for deploy.', function () {
-      var options, done = this.async();
+    var options, done = this.async();
 
-      options = ['--exclude'].concat(this.data.exclude);
-      options = options.concat(['-r', this.data.dest, this.data.src]);
-      
-      grunt.utils.spawn({cmd: 'zip', args: options}, function (err, result, code) {
-          var output = result.stderr? result.stderr: result.stdout;
-          grunt.log.writeln( '\n'+output+'\n' );
-          
-          done(code>0? false: true);
-      });
+    options = ['--exclude'].concat(this.data.exclude);
+    options = options.concat(['-r', this.data.dest, this.data.src]);
+    
+    grunt.utils.spawn({cmd: 'zip', args: options}, function (err, result, code) {
+		  grunt.helper('writeOutput', result.err, result.stdout, code);
+
+			done(code>0? false: true);
+    });
   });
 
 
   grunt.registerTask('test', 'Run specs using npm test', function () {
-      var done = this.async();
+    var done = this.async();
 
-      grunt.utils.spawn({ cmd: 'npm', args: ['test'] }, function (err, result, code) {
-          var output = result.stderr? result.stderr: result.stdout;
-          grunt.log.writeln( '\n'+output+'\n' );
-          
-          done( code>0? false: true );
-      });
+    grunt.utils.spawn({ cmd: 'npm', args: ['test'] }, function (err, result, code) {
+		  grunt.helper('writeOutput', result.err, result.stdout, code);
+      
+      done(code>0? false: true);
+    });
+  });
+
+
+	// Helpers
+	
+	grunt.task.registerHelper("writeOutput", function (errorMsg, successMsg, code) {
+    var output = errorMsg || successMsg;
+
+    grunt.log.writeln( '\n'+output+'\n' );
   });
 };
