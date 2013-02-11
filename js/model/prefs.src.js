caecc64cd741788596a337cc35b1fe23a254cfac
Leonardo Quixada <lquixada@gmail.com>
2013-02-11 00:21:46 -0200

    Prefs: managing multiple users.

--- js/model/prefs.src.js
+++ js/model/prefs.src.js
@@ -2,10 +2,45 @@ Preferences = o.clazz({
 	extend: Model,
 	key: 'prefs',
 
+	addUser: function (user) {
+		var users = this.get('users');
+
+		users = users? users.split(','): [];
+
+		if (users.indexOf(user)>-1) {
+			return;
+		}
+
+		// Add to the front
+		users.unshift(user.trim());
+
+		this.set('users', users.join(','));
+	},
+
 	get: function (key) {
 		var prefs = this._super() || {};
 	 
 		return key? prefs[key]: prefs;
+	},
+
+	getUsers: function () {
+		var users = this.get('users');
+		
+		if (users) {
+			return users.split(',');
+		} else {
+			return [];
+		}
+	},
+
+	removeUser: function (user) {
+		var users = this.getUsers();
+
+		users = users.filter(function (element) {
+			return (element !== user);
+		});
+	
+		this.set('users', users.join(','));
 	},
 
 	set: function (attrKey, attrValue) {
--- specs/model/prefs.spec.js
+++ specs/model/prefs.spec.js
@@ -1,28 +1,110 @@
 describe("Preferences Model", function() {
-    afterEach(function() {
-        delete localStorage.prefs;
-    });
-    
-    it("should not have stored preferences", function() {
-        expect(Prefs.get()).toBeEmptyObject();
-    });
+	afterEach(function() {
+		delete localStorage.prefs;
+	});
 
-    it("should store preferences on 'prefs' key", function() {
-        Prefs.set('user', 'test');
+	describe("set", function() {
+		it("should store preference on 'prefs' key", function() {
+			Prefs.set('user', 'test');
 
-        expect(localStorage.prefs).toBe(JSON.stringify({'user': 'test'}));
-    });
+			expect(localStorage.prefs).toBe('{"user":"test"}');
+		});
 
-    it("should get the right preferences", function() {
-        Prefs.set('user', 'test');
+		it("should get all the preferences", function() {
+			Prefs.set('user', 'test');
+			Prefs.set('password', 'wow');
 
-        expect(Prefs.get()).toBeJson({'user': 'test'});
-    });
+			expect(Prefs.get()).toBeJson({user: 'test', password: 'wow'});
+		});
+	});
+	
+	describe("get", function() {
+		it("should get an empty object", function() {
+			expect(Prefs.get()).toBeEmptyObject();
+		});
 
-    it("should the value of the key", function() {
-        Prefs.set('user', 'test');
+		it("should get the value of a key", function() {
+			localStorage.prefs = '{"user": "test"}';
 
-        expect(Prefs.get('user')).toBeJson('test');
-    });
+			expect(Prefs.get('user')).toBe('test');
+		});
+
+		it("should get all the preferences", function() {
+			localStorage.prefs = '{"user": "test", "password": "wow"}';
+
+			expect(Prefs.get()).toBeJson({user:'test', password: 'wow'});
+		});
+	});
+
+	describe("set and get", function() {
+		it("should get what is set", function() {
+			Prefs.set('user', 'test');
+
+			expect(Prefs.get('user')).toBe('test');
+			expect(Prefs.get()).toBeJson({user: 'test'});
+		});
+	});
+
+	describe("addUser", function() {
+		it("should store one user", function() {
+			Prefs.addUser('john');
+
+			expect(Prefs.get('users')).toBe('john');
+		});
+
+		it("should store two users in reverse order", function() {
+			Prefs.addUser('john');
+			Prefs.addUser('bill');
+			
+			expect(Prefs.get('users')).toBe('bill,john');
+		});
+		
+		it("should not store the same user twice", function() {
+			Prefs.addUser('john');
+			Prefs.addUser('john');
+			
+			expect(Prefs.get('users')).toBe('john');
+		});
+	});
+	
+	describe("getUsers", function() {
+		it("should be an empty array", function() {
+			expect(Prefs.getUsers()).toBeArray();
+			expect(Prefs.getUsers().length).toBe(0);
+		});
+
+		it("should be a list of users", function() {
+			var list;
+
+			Prefs.addUser('john');
+			Prefs.addUser('bill');
+			
+			list = Prefs.getUsers();
+
+			expect(list.length).toBe(2);
+			expect(list[0]).toBe('bill');
+			expect(list[1]).toBe('john');
+		});
+	});
+	
+	describe("removeUser", function() {
+		it("should remove a user", function() {
+			Prefs.addUser('john');
+			Prefs.addUser('bill');
+
+			Prefs.removeUser('bill');
+
+			expect(Prefs.get('users')).toBe('john');
+		});
+
+		it("should maintain list if user doesn't exist", function() {
+			Prefs.addUser('john');
+			Prefs.addUser('bill');
+
+			Prefs.removeUser('ted');
+
+			expect(Prefs.get('users')).toBe('bill,john');
+		});
+	});
 });
 
