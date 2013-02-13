2e8c064920a9125bf9b1ef401e124be971283c23
Leonardo Quixada <lquixada@gmail.com>
2013-02-13 12:55:36 -0200

    added query feature to el method

--- js/controller/controller.src.js
+++ js/controller/controller.src.js
@@ -3,9 +3,13 @@ Controller = o.clazz( {
 		throw 'not implemented error';
 	},
 
-	el: function () {
+	el: function (query) {
 		if (!this.element) {
 			this.element = this.$(this.dom);
+		}
+
+		if (query) {
+			return this.element.querySelector(query);
 		}
 		
 		return this.element;
