3e52ce7820a2572f2e5fdf97cf5702716ff5c262
Leonardo Quixada <lquixada@gmail.com>
2013-02-23 00:10:32 -0300

    specs folder renamed to spec, as matchers.js renamed to spec.src.js. Fetching fixture before running specs.

--- spec/spec.src.js
+++ spec/spec.src.js
@@ -1,4 +1,19 @@
+var fixture = '';
+	
+// Getting html fixture before suite spec begins
+$.ajax({
+	url: 'html/popup.html',
+	dataType: 'html',
+	async: false,
+	success: function (data) {
+		// Extract all body markup
+		fixture = data.match(/<body>([\s\S]*)<\/body>/)[1];
+	}
+});
+
 beforeEach(function() {
+	$('div#main').html(fixture);
+
   this.addMatchers({
     toBeArray: function () {
       return toString.call(this.actual) === '[object Array]';
