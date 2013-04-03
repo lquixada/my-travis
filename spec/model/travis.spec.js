0f624cdd61fb49bd7bbc965b44bf4470020ae4f2
Leonardo Quixada <lquixada@gmail.com>
2013-04-02 21:28:33 -0300

    TravisAPI: better spec.

--- spec/model/travis.spec.js
+++ spec/model/travis.spec.js
@@ -1,14 +1,9 @@
 describe("TravisAPI", function() {
-	var callback;
-
-	beforeEach(function() {
-		callback = jasmine.createSpy('onComplete');
-			
-		spyOn($, 'getJSON');
-	});
-	
 	it("should make a request to TravisCI with callback", function() {
+		var callback = jasmine.createSpy('onComplete');
 		var url = 'https://api.travis-ci.org/repos?owner_name[]=user1&owner_name[]=user2';
+
+		spyOn($, 'getJSON');
 
 		TravisAPI.get(['user1', 'user2'], callback);
 
