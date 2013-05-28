6f5d465bca6f889317b45beb5809db1e87d2cdea
Leonardo Quixada <lquixada@gmail.com>
2013-05-28 16:31:20 -0300

    Status converted to Backbone.Model

--- app/app.src.js
+++ app/app.src.js
@@ -43,49 +43,6 @@ var DOMController = o.Class( {
 }); 
 
 
-var Model = o.Class( {
-	clear: function () {
-		throw 'not implemented error';
-	},
-
-	get: function () {
-		throw 'not implemented error';
-	},
-
-	set: function (value) {
-		throw 'not implemented error';
-	}
-});
-
-
-var ModelLocalStorage = o.Class( {
-	extend: Model,
-
-	clear: function () {
-		delete localStorage[this._getKey()];
-	},
-
-	get: function () {
-		var value = localStorage[this._getKey()];
-		return (value? JSON.parse(value): null);
-	},
-
-	set: function (value) {
-		localStorage[this._getKey()] = JSON.stringify(value);
-	},
-
-	// private
-
-	_getKey: function () {
-		if (!this.key) {
-			throw 'key not specified error.';
-		}
-
-		return this.key;
-	}
-});
-
-
 var Source = o.Class({
 	get: function () {
 		throw 'not implemented error';
--- app/model/status.src.js
+++ app/model/status.src.js
@@ -1,22 +1,7 @@
-/*globals ModelLocalStorage */
+// Backbone
 
-var StatusModel = o.Class({
-	extend: ModelLocalStorage,
-	key: 'statuses',
-
-	get: function (key) {
-		var prefs = this._super() || {};
-	 
-		return key? prefs[key]: prefs;
-	},
-
-	set: function (attrKey, attrValue) {
-		var prefs = this.get();
-		
-		prefs[attrKey] = attrValue;
-
-		this._super(prefs);
-	}
+var StatusModel = Backbone.Model.extend({
+	localStorage: new Backbone.LocalStorage('Statuses')
 });
 
 var Status = new StatusModel();
